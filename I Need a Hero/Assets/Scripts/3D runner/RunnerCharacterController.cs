849c1d420f0aa64f3b0ec4381e069f6b9e771392
Axel <Axel.Thomsen.0446@student.uu.se>
2021-03-09 19:07:11 +0100

    really simple versions of everything

--- I Need a Hero/Assets/Scripts/3D runner/RunnerCharacterController.cs
+++ I Need a Hero/Assets/Scripts/3D runner/RunnerCharacterController.cs
@@ -21,6 +21,8 @@ public class RunnerCharacterController : MonoBehaviour
     float jumpTimer;
     float jumpGracePeriod = 0.2f;
 
+    bool gameStarted = false;
+
 
     // Start is called before the first frame update
     void Start()
@@ -33,8 +35,8 @@ public class RunnerCharacterController : MonoBehaviour
     {
         //Check if grounded
         isGrounded = false;
-        
-        foreach(var groundCheck in groundChecks)
+
+        foreach (var groundCheck in groundChecks)
         {
             if (Physics.CheckSphere(transform.position, 0.1f, groundLayers, QueryTriggerInteraction.Ignore))
             {
@@ -42,7 +44,7 @@ public class RunnerCharacterController : MonoBehaviour
                 break;
             }
         }
-        
+
 
         if (isGrounded && velocity.y < 0)
         {
@@ -54,6 +56,20 @@ public class RunnerCharacterController : MonoBehaviour
             velocity.y += gravity * Time.deltaTime;
         }
 
+
+        if (gameStarted)
+        {
+            SideMovement();
+            Jump();
+            RunForward();
+        }
+        AssignGravity();
+
+        StartGame();
+    }
+
+    private void Jump()
+    {
         //Jump
         jumpPressed = Input.GetButtonDown("Jump");
         if (jumpPressed)
@@ -66,13 +82,32 @@ public class RunnerCharacterController : MonoBehaviour
             velocity.y += Mathf.Sqrt(jumpHeight * -2 * gravity);
             jumpTimer = -1;
         }
+    }
 
+    void StartGame()
+    {
+        if (!gameStarted && Input.anyKeyDown)
+        {
+            gameStarted = true;
+        }
+    }
+
+    void SideMovement()
+    {
         if (isGrounded && Input.GetAxisRaw("Horizontal") != 0)
         {
             characterController.Move(new Vector3(Input.GetAxisRaw("Horizontal") * strafeSpeed * Time.deltaTime, 0, 0));
         }
+    }
 
+    void RunForward()
+    {
+        //Constant run
         characterController.Move(new Vector3(0, 0, forwardInput * runSpeed) * Time.deltaTime);
+    }
+
+    void AssignGravity()
+    {
         //Vertical velocity
         characterController.Move(velocity * Time.deltaTime);
     }
